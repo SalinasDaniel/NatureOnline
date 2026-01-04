c3d650b434763305ed77f551ea46e61d70208d3e
Daniel Salinas <41959585+SalinasDaniel@users.noreply.github.com>
2026-01-04 14:18:10 -0800

    Delete src directory

--- src/layout/meta.php
+++
@@ -1,9 +0,0 @@
-<?php $basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/') . '/'; ?>
-
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<base href="<?= $basePath ?>">
-<link rel="icon" href="assets/favicon/hoja.png" type="image/x-icon">
-<link rel="stylesheet" href="css/main.css">
-<link rel="stylesheet" href="vendor/bootstrap-5.3.3/css/bootstrap.min.css">
-<link href="vendor/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
